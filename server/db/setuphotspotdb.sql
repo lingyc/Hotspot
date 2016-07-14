a8d2ba468110953b84d993ce24022512c71ecd8e
Alex Stark <alexcstark@gmail.com>
2016-07-14 08:17:07 -0700

    troubleshoot db

--- server/db/setuphotspotdb.sql
+++ server/db/setuphotspotdb.sql
@@ -13,14 +13,14 @@ CREATE TABLE spots (
   latitude REAL,
   longitude REAL,
   image VARCHAR,
-  spots_users_id references spots_users_id(id)
+  spots_users_id INT references spots_users_id(id)
 );
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username VARCHAR,
   password VARCHAR,
-  spots_users_id references spots_users_id(id)
+  spots_users_id INT references spots_users_id(id)
 );
 
 CREATE TABLE spots_users (
@@ -29,12 +29,9 @@ CREATE TABLE spots_users (
   spotId INT
 );
 
-
-
-
 INSERT INTO spots (name, description, city, latitude, longitude, image)
-  VALUES ('Dads Deck', 'Nice, homey, and outdoors', 'San Leandro', '37.5', '-112', '/boston.jpg');
+  VALUES ('Dads Deck', 'Nice, homey, and outdoors', 'San Leandro', '37.5', '-112', '/boston.jpg', 1);
 INSERT INTO spots (name, description, city, latitude, longitude, image)
-  VALUES ('Spot 2', 'Rad', 'San Leandro', '100', '-50', '/library.jpg');
+  VALUES ('Spot 2', 'Rad', 'San Leandro', '100', '-50', '/library.jpg', 1);
 INSERT INTO spots (name, description, city, latitude, longitude, image)
-  VALUES ('Spot 3', 'Sick...and tight', 'Paris', '37.5', '30',  '/study2.jpg');
+  VALUES ('Spot 3', 'Sick...and tight', 'Paris', '37.5', '30',  '/study2.jpg', 1);
