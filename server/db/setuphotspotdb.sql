daba5201dfeab3156f43858b9938dd1eb1e1ce46
Alex Stark <alexcstark@gmail.com>
2016-07-14 16:36:12 -0700

    additional dummy data

--- server/db/setuphotspotdb.sql
+++ server/db/setuphotspotdb.sql
@@ -31,14 +31,26 @@ CREATE TABLE users (
 INSERT INTO spots_users (userId, spotId)
   VALUES (1, 1);
 
+INSERT INTO spots_users (userId, spotId)
+  VALUES (1, 2);
+
+INSERT INTO spots_users (userId, spotId)
+  VALUES (2, 1);
+
+INSERT INTO spots_users (userId, spotId)
+  VALUES (2, 3);
+
 INSERT INTO users (username, password, spots_users_id)
   VALUES ('alex', 'password', 1);
+
+INSERT INTO users (username, password, spots_users_id)
+  VALUES ('ryan', 'password', 2);
 
 INSERT INTO spots (name, description, latitude, longitude, image, spots_users_id)
   VALUES ('Dads Deck', 'Nice, homey, and outdoors', '37.5', '-112', '/boston.jpg', '1');
 
 INSERT INTO spots (name, description, latitude, longitude, image, spots_users_id)
-  VALUES ('Spot 2', 'Rad', '100', '-50', '/library.jpg', '1');
+  VALUES ('Hack Reactor', 'Rad', '100', '-50', '/library.jpg', '1');
 
 INSERT INTO spots (name, description, latitude, longitude, image, spots_users_id)
   VALUES ('Spot 3', 'Sick...and tight', '37.5', '30',  '/study2.jpg', '1');
