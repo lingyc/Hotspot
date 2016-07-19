3976a83b54f67d9fdf3df26384c3499a3562559e
andrew-heath <aheath42@gmail.com>
2016-07-18 19:50:11 -0700

    Make action list outline

---
+++ Client/actions/index.jsx
@@ -0,0 +1,53 @@
+import $ from 'jquery';
+
+/*
+ ===== Action List =====
+  nav-collection-toggle
+  nav-filter
+  nav-logout
+
+  panel-filter-item-toggle
+  panel-collection-item-open
+  panel-collection-item-close
+
+  map-place-point
+  map-rating
+*/
+
+export const NAV_CLICK_COLLECTION = 'NAV_CLICK_COLLECTION';
+export const NAV_CLICK_FILTER = 'NAV_CLICK_FILTER';
+export const NAV_CLICK_LOGOUT = 'NAV_CLICK_LOGOUT';
+
+export const PANEL_CLICK_FILTER_ITEM = 'PANEL_CLICK_FILTER_ITEM';
+export const PANEL_OPEN_COLLECTION_ITEM = 'PANEL_OPEN_COLLECTION_ITEM';
+export const PANEL_CLOSE_COLLECTION_ITEM = 'PANEL_CLOSE_COLLECTION_ITEM';
+
+// Click Handler for Nav Collection button
+export function toggleCollectionList() {
+
+}
+
+// Click Handler for Nav Filter button
+export function toggleFilterList() {
+
+}
+
+// Click Handler for Nav Logout button
+export function logout() {
+
+}
+
+// Click Handler for Panel Filter item
+export function toggleFilter(filter) {
+
+}
+
+// Click Handler for Panel Collection item
+export function viewCollectionItem(item) {
+
+}
+
+// Click Handler for Panel Collection closeup
+export function closeCollectionItem(item) {
+
+}
