6891e2fc9f2ce9b44eff03e6ed535b01d46c904d
Ryan McChesney <rmcchesn@gmail.com>
2016-07-18 20:02:37 -0700

    Add default styling to index.html - to be moved to main css file later. Update default coordinates on mapbox.

--- Client/containers/Map.jsx
+++ Client/containers/Map.jsx
@@ -1,3 +1,5 @@
+var defaultCoord = [37.7837008, -122.4111551];
+
 L.mapbox.accessToken = 'pk.eyJ1Ijoicm1jY2hlc24iLCJhIjoiY2lxbHkxbXFiMDA5dWZubm5mNWkwdGYwbiJ9.QC1lP-2tNymbJ5tHaMugZw';
 import React from 'react';
 import { connect } from 'react-redux';
@@ -7,7 +9,7 @@ import * as Actions from '../actions';
 class Map extends React.Component {
 
   componentDidMount() {
-    this.map = L.mapbox.map('map-one', 'mapbox.streets').setView([38.8929, -77.0252], 14);
+    this.map = L.mapbox.map('map-one', 'mapbox.streets').setView(defaultCoord, 14);
   }
 
   render() {
