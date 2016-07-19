83d92d5866f46b53887614e4f539804989831a6d
andrew-heath <aheath42@gmail.com>
2016-07-19 00:41:00 -0700

    Populate actions with basic actions and pseudocode.  Certain specific actions pending.

--- Client/actions/index.jsx
+++ Client/actions/index.jsx
@@ -23,31 +23,78 @@ export const PANEL_OPEN_COLLECTION_ITEM = 'PANEL_OPEN_COLLECTION_ITEM';
 export const PANEL_CLOSE_COLLECTION_ITEM = 'PANEL_CLOSE_COLLECTION_ITEM';
 
 // Click Handler for Nav Collection button
-export function toggleCollectionList() {
+export function toggleCollectionList(panelMode) {
+  // If panelMode is collection, set it to null.
+  if (panelMode === 'collection') {
+    panelMode = null;
+  } else {
+    // Else set panelMode to collection
+    panelMode = 'collection';
+  }
 
+  return {
+    type: NAV_CLICK_COLLECTION,
+    payload: panelMode
+  }
 }
 
 // Click Handler for Nav Filter button
-export function toggleFilterList() {
+export function toggleFilterList(panelMode) {
+  // If panelMode is filter, set it to null.
+  if (panelMode === 'filter') {
+    panelMode = null;
+  } else {
+    // Else set panelMode to filter
+    panelMode = 'filter';
+  }
 
+  return {
+    type: NAV_CLICK_FILTER,
+    payload: panelMode
+  }
 }
 
 // Click Handler for Nav Logout button
 export function logout() {
+  // Make final post request to update user's data
+  // End the user's session
 
+  return {
+    type: NAV_CLICK_LOGOUT
+  }
 }
 
 // Click Handler for Panel Filter item
-export function toggleFilter(filter) {
+export function toggleFilter(filter, appliedFilters) {
+  // Check if given filter is in filter list
+  var index = _.findIndex(appliedFilters, filter);
+  if (index === -1) { 
+    // Add it to the list if not found
+    appliedFilters.push(filter);
+  } else {
+    // remove it if it is not
+    appliedFilters.splice(index, 1);
+  }
 
+  return {
+    type: PANEL_CLICK_FILTER_ITEM,
+    payload: appliedFilters
+  }
 }
 
 // Click Handler for Panel Collection item
 export function viewCollectionItem(item) {
-
+  // change current panel view to the collection item
+  return {
+    type: PANEL_OPEN_COLLECTION_ITEM,
+    payload: item
+  }
 }
 
 // Click Handler for Panel Collection closeup
 export function closeCollectionItem(item) {
-
+  // close the current panel view back to the collection
+  return {
+    type: PANEL_CLOSE_COLLECTION_ITEM
+  }
 }
