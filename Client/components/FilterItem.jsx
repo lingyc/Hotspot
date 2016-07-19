be121bcd1c0494530930f0c06ff227175fd88383
andrew-heath <aheath42@gmail.com>
2016-07-19 00:51:22 -0700

    Organize FilterItem

---
+++ Client/components/FilterItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// The contets will be dependant on the form the information takes
+var FilterItem = ({filter, appliedFilters, toggleFilter}) => {
+  var cssClasses = 'filterName';
+
+  if (_.findIndex(appliedFilters, filter) > -1) {
+    cssClasses += ' ' + 'toggleOn';
+  }
+  return (
+    <div className='filter'>
+      <div className={cssClasses} onClick={ () => { toggleFilter(filter, appliedFilters) }}>
+      {item}
+      </div>
+    </div>
+  );
+}
+
+export default CollectionModel;
