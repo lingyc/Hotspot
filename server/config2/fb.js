ace11486ce2a1e3515c0d416b2f411d8eb218b5d
Alex Stark <alexcstark@gmail.com>
2016-07-20 11:13:44 -0700

    remove config2

--- server/config2/fb.js
+++
@@ -1,3 +0,0 @@
- export const FACEBOOK_APP_ID = 312037755796098;
- export const FACEBOOK_APP_SECRET = 'c478d8a2b4711fe5137cd404b39fdef7';
- export const FACEBOOK_CALLBACK = 'http://localhost:8000/auth/facebook/callback';
