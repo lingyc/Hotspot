f9ca9dfe0d7337f238b017ab205767d6cd49efed
andrew-heath <aheath42@gmail.com>
2016-07-21 16:44:36 -0700

    Add store to index.js

--- Client/index.js
+++ Client/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import configureStore from './store/storeConfig.jsx';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
   </Provider>,
   document.getElementById('app')
 );
