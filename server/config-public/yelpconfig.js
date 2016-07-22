c6967fc4290b70159a447f4480c76a28c3e778ca
Alex Stark <alexcstark@gmail.com>
2016-07-22 11:47:13 -0700

    more build

--- server/config-public/yelpconfig.js
+++ server/config-public/yelpconfig.js
@@ -1,4 +1,6 @@
-export const YELP_CONSUMER_KEY = 0;
-export const YELP_CONSUMER_SECRET = 0;
-export const YELP_TOKEN = 0;
-export const YELP_TOKEN_SECRET = 0;
+export default {
+  YELP_CONSUMER_KEY: 0,
+  YELP_CONSUMER_SECRET: 0,
+  YELP_TOKEN: 0,
+  YELP_TOKEN_SECRET: 0
+};
