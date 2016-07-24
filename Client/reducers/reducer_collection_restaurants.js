0a6b3a65a3110cac493c0be1412052b9598450b2
andrew-heath <aheath42@gmail.com>
2016-07-23 19:39:05 -0700

    Fix syntax error in object referencing, added delete handler for reducer_collection_restaurants

--- Client/reducers/reducer_collection_restaurants.js
+++ Client/reducers/reducer_collection_restaurants.js
@@ -1,6 +1,6 @@
 import { MAP_CONFIRM_POINT } from '../actions/index';
 import { FETCH_RESTAURANTS } from '../actions/index';
-import { POPULATE_FILTER_OPTIONS } from '../actions/index';
+import { PANEL_DELETE_COLLECTION_ITEM } from '../actions/index';
 
 const initialState = {
   filterOptions: [],
@@ -9,15 +9,24 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
-  case MAP_CONFIRM_POINT:
-    return {
-      ...state,
-      filterOptions: state.filterOptions.concat([action.payload.filters])
-    };
-  case FETCH_RESTAURANTS:
-    return {
-      ...state,
-      collection: action.payload
+    case MAP_CONFIRM_POINT:
+      return {
+        ...state,
+        filterOptions: action.payload.filters,
+        collection: state.collection.concat(action.payload.newSpot)
+      };
+    case FETCH_RESTAURANTS:
+      return {
+        ...state,
+        filterOptions: action.payload.filters,
+        collection: action.payload.collection
+      };
+    case PANEL_DELETE_COLLECTION_ITEM:
+      return {
+        ...state,
+        filterOptions: action.payload.filters,
+        collection: action.payload.collection
+      }
     default:
       return state;
   }
