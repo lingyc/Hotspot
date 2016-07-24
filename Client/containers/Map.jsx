4abe8c2332305eb424d5c69a506565ffdae3f853
andrew-heath <aheath42@gmail.com>
2016-07-23 20:03:09 -0700

    Update Map container to use action and state

--- Client/containers/Map.jsx
+++ Client/containers/Map.jsx
@@ -1,11 +1,10 @@
 L.mapbox.accessToken = 'pk.eyJ1Ijoicm1jY2hlc24iLCJhIjoiY2lxbHkxbXFiMDA5dWZubm5mNWkwdGYwbiJ9.QC1lP-2tNymbJ5tHaMugZw';
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as Actions from '../actions';
 
 class Map extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
 
   componentDidMount() {
     this.map = L.mapbox.map('map-one', 'mapbox.streets').setView([38.8929, -77.0252], 14);
@@ -13,10 +12,21 @@ class Map extends React.Component {
 
   render() {
     return (
-      <div className='map' id='map-one'></div>
+      <div className='map' id='map-one' filters={this.props.filters} clickHandler={this.props.actions.clickLocationSubmit.bind(this)}></div>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return {
+    filters: state.CollectionRestaurantsFilters.filterOptions
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    action: bundActionCreators(Actions, dispath)
+  };
+}
 
 export default Map;
