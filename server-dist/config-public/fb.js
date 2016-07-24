e98fd181117696d39e3809a376588f90a3d9a12f
Alex Stark <alexcstark@gmail.com>
2016-07-23 22:12:26 -0700

    remove server dist

--- server-dist/config-public/fb.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var FACEBOOK_APP_ID = exports.FACEBOOK_APP_ID = 0;
-var FACEBOOK_APP_SECRET = exports.FACEBOOK_APP_SECRET = 0;
--- server-dist/config-public/inLocalEnv.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-exports.default = function () {
-  return fs.readdirSync(path.join(__dirname)).indexOf('config') >= 0;
-};
