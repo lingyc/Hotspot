5f228833bad143b160ff8e8ab2a590c1046d343b
andrew-heath <aheath42@gmail.com>
2016-07-24 22:07:44 -0700

    Attempt at organizing and styling information to be visible

--- Client/actions/index.jsx
+++ Client/actions/index.jsx
@@ -163,9 +163,54 @@ export function clickLocationSubmit(name, latitude, longitude, rating) {
 export function fetchCollection() {
   // This function should only be called once on startup
   // Query database for user's entire collection
-  const collection = request.get(endpoints.spots);
-  const filters = filterOrganizer(collection);
+  // const collection = request.get(endpoints.spots);
+  const collection = [
+    {
+      name: 'The Flying Falafal',
+      latitude: 37.7812322,
+      longitude: -122.4134787,
+      rating: 5,
+      type: 'middle-east'
+    },
+    {
+      name: 'Show Dogs',
+      latitude: 37.7821228,
+      longitude: -122.4130593,
+      rating: 5,
+      type: 'american'
+    },
+    {
+      name: 'Lemonade',
+      latitude: 37.7848661,
+      longitude: -122.4057182,
+      rating: 5,
+      type: 'drink'
+    },
+    {
+      name: 'Super Duper Burgers',
+      latitude: 37.7862143,
+      longitude: -122.4053212,
+      rating: 5,
+      type: 'american'
+    },
+    {
+      name: 'Réveille Coffee Co.',
+      latitude: 37.7735341,
+      longitude: -122.3942448,
+      rating: 5,
+      type: 'coffee'
+    },
+    {
+      name: 'Denny\'s',
+      latitude: 37.7859249,
+      longitude: -122.407801,
+      rating: 0,
+      type: 'american'
+    }
+  ];
 
+  const filters = filterOrganizer(collection);
+  
   return {
     type: FETCH_COLLECTION,
     payload: {
@@ -179,8 +224,8 @@ const filterOrganizer = (collection, filters) => {
   filters = filters || [];
 
   _.map(collection, (value) => {
-    if (_.findIndex(filters, value) === -1) {
-      filters.push(value);
+    if (_.findIndex(filters, value.type) === -1) {
+      filters.push(value.type);
     }
   });
 
