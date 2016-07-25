95f05e2cfade4ec65ab2dae418ce14f1881d723c
andrew-heath <aheath42@gmail.com>
2016-07-24 20:58:16 -0700

    Redumentary styling for buttons and panel

--- Client/containers/Nav.jsx
+++ Client/containers/Nav.jsx
@@ -23,9 +23,9 @@ class Nav extends React.Component {
   render() {
     return (
       <nav className="navbar">
-          <div onClick={this.collectionClick.bind(this)} className='collectionPanel' >Collection</div>
-          <div onClick={this.filterClick.bind(this)} className='filterPanel'>Filter</div>
-          <a href="/logout">Sign Out</a>
+          <div onClick={this.collectionClick.bind(this)} className='btn btn-default' >Collection</div>
+          <div onClick={this.filterClick.bind(this)} className='btn btn-default'>Filter</div>
+          <a className='btn btn-default' href="/logout">Sign Out</a>
       </nav>
     );
   }
