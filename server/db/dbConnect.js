dd8e2e8b39ae5cf14caa11f4dfa9622676e90c2b
justin-lai <justin.th.lai@gmail.com>
2016-07-27 15:53:57 -0700

    Arb commit

--- server/db/dbConnect.js
+++ server/db/dbConnect.js
@@ -4,11 +4,10 @@ const pgp = require('pg-promise')({
   promiseLib: promise
 });
 
-let connectionString = 'postgres://eddjkiejqgentb:SmKRv-lNqBN1O1O_yL_jbCug_Q@ec2-50-17-255-49.compute-1.amazonaws.com:5432/dbcptdm6hun6jj';
-
-if (process.env.DATABASE_URL) {
-  connectionString = process.env.DATABASE_URL;
-}
+let connectionString = 'postgres://127.0.0.1:5432/hotspots';
+// if (process.env.DATABASE_URL) {
+//   connectionString = process.env.DATABASE_URL;
+// }
 
 const pg = pgp(connectionString);
 
