30c69b511783750cc8e10ccd8815e338b9342fe9
justin-lai <justin.th.lai@gmail.com>
2016-07-27 14:04:53 -0700

    Change db url

--- server/db/dbConnect.js
+++ server/db/dbConnect.js
@@ -1,10 +1,10 @@
 import promise from 'bluebird';
-
+console.log(process.env.DATABASE_URL);
 const pgp = require('pg-promise')({
   promiseLib: promise
 });
 
-let connectionString = 'postgres://localhost:5432/hotspots';
+let connectionString = 'postgres://eddjkiejqgentb:SmKRv-lNqBN1O1O_yL_jbCug_Q@ec2-50-17-255-49.compute-1.amazonaws.com:5432/dbcptdm6hun6jj';
 
 if (process.env.DATABASE_URL) {
   connectionString = process.env.DATABASE_URL;
