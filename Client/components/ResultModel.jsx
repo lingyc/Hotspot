1676ed0598cb0eab5e7a079930a2bd5a4fd13aee
Piquant-Toothbrush <krishanmarya1@gmail.com>
2016-07-29 20:59:08 -0700

    Render more addresses

--- Client/components/ResultModel.jsx
+++ Client/components/ResultModel.jsx
@@ -13,6 +13,7 @@ var ResultModel = ({item}) => {
   console.log('item',item)
     var url=item.url
     var rating=item.rating;
+    var address=item.address;
 
   return (
   <div id='restaurant' className='restaurant card' >
@@ -21,6 +22,7 @@ var ResultModel = ({item}) => {
       <h4 className='card-title'><a href={url} target="_blank">{item.name}</a></h4>
     </div>
     <ul className='list-group list-group-flush'>
+        <li className='list-group-item'>Address: <p style={{fontSize:'12px'}}>{address}</p></li>
       <li className='list-group-item'>Yelp Rating: {rating}</li>
       <li className='list-group-item'>Type: {item.cuisine}</li>
     </ul>
