419fa524e0660a19137b4e59e5a7f6845488f32b
Ling Yu Chen <lingchen215@gmail.com>
2016-08-01 13:51:27 -0700

    Change db url

--- server/db/dbConnect.js
+++ server/db/dbConnect.js
@@ -4,7 +4,8 @@ const pgp = require('pg-promise')({
   promiseLib: promise
 });
 
-let connectionString = 'postgres://127.0.0.1:5432/hotspots'; 
+let connectionString = 'postgres://inxloxepkkypkj:alOq4htfCdEjNUDGoXqeZ0S8QF@ec2-54-235-254-56.compute-1.amazonaws.com:5432/d1kgfr0gstdck2';
+// let connectionString = 'postgres://127.0.0.1:5432/hotspots'; 
 if (process.env.DATABASE_URL) {
   connectionString = process.env.DATABASE_URL;
 }
